066fdbbb66bd95cb40bae055ad0968d0f5b89ba8
Ims4d <156629837+Ims4d@users.noreply.github.com>
2025-02-12 15:36:11 +0700

    up

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,11 @@ from scipy.stats import f_oneway
 st.set_page_config(
     page_title="Dashboard Analisis Data Pengiriman Pesanan",
     layout="wide",
-    initial_sidebar_state="expanded"
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
 )
 
 # Sidebar Navigasi
